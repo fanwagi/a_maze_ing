ebc5442b3762370a7bf5303d6b8844f11bb7caa3
fanw <fanwu515@gmail.com>
2024-08-18 17:26:20 +0100

    added comments.

--- src/room.py
+++ src/room.py
@@ -2,12 +2,19 @@ from typing import List, Tuple, Dict
 
 
 class Room:
+    """
+    A room in the maze, represented by the start location (a tuple of 3 values), the size on the three dimensions
+    (a tuple of 3 values), and its unique id.
+    """
     def __init__(self, loc: Tuple[int, int, int], size: Tuple[int, int, int], id: int):
         self.loc = loc
         self.size = size
         self.id = id
+        # a list of it's neighbours that share a wall with this room
         self.neighbors: List[Room] = []
+        # the directions of all its neighbours, it can be up/down/left/right/forward/backward
         self.neighbors_directions: List[int] = []
+        # a list of doors, represented as a dictionary of neighbour-room: (door location cell, direction)
         self.doors: Dict[Room, Tuple[Tuple[int, int, int], int]] = {}
 
     def __hash__(self):
